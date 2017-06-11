4d68be29a2f93d78a5237b3bce7a7a0df88174f7
igoldyrev <goldirev12@gmail.com>
2017-06-11 15:22:57 +0500

    Добавлена новость с расписанием работы 12 июня

---
+++ news/raspisanie_12_june.php
@@ -0,0 +1,22 @@
+<?php include ($_SERVER["DOCUMENT_ROOT"]."/frames/headtags.php"); 
+echo "<title>$titleconst Расписание работы 12 июня</title>";
+?>
+
+<meta name="description" content="Продажа и прокат багажников и автобоксов THULE, Mont Blanc, Amos, Atera, Атлант в Перми. Пермь, ул. Спешилова 102, павильон 29.">
+<meta name="keywords" content="автобагажники, автомобильные багажники, багажники для автомобиля, прокат, прокат багажников, прокат боксов, багажные системы, багажник на крышу, автобагажники на крышу, автомобильные боксы, автобоксы, автобокс, бокс, бокс на крышу, автобокс на крышу, велобагажники, велокрепления, багажники для велосипедов, велокрепления на крышу, велокрепления на фаркопы, крепления для лыж, крепления для сноубордов, рейлинги, интернет-магазин, продажа в Перми, Пермь, Автобагаж, в Перми, thule, туле, Yuago, ЯГО, Турино, Lux, Люкс, Атлант, Discovery, Amos, Амос, Mont Blanc, Монблан, Can, Voyager, Cosmo, Муравей, Атера, Atera, menabo"> 
+</head>
+<body>
+<?php include ($_SERVER["DOCUMENT_ROOT"]."/frames/header.html");?>
+<div id="leftmenu">
+<?php include ($_SERVER["DOCUMENT_ROOT"]."/frames/leftmenu.html");?>
+</div>
+<div id="content">
+<h1>Расписание работы 12 июня</h1>
+<p align="center">Уважаемые клиенты!</p>
+<p>Рады Вам сообщить, что, несмотря на праздник, наш магазин работает с 10-00 до 16-00.</p>
+<div class="img_div">
+<img class="img_main" src="/images/news/110617/12june.jpg" srcset="
+/images/news/110617/12june.jpg 350w" sizes="(max-width: 2000px) 150px, 300px, 350px" alt="День России">
+</div></div>
+<?php include ($_SERVER["DOCUMENT_ROOT"]."/frames/footer.html");?>
+<?php include ($_SERVER["DOCUMENT_ROOT"]."/frames/counters.html");?>
