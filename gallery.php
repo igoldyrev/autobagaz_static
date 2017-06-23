7550d00381ce9efecf46ad5d5ba1bd311dbc4983
igoldyrev <goldirev12@gmail.com>
2017-06-23 13:15:16 +0500

    Добавлен пустой файл галереи для сайта

---
+++ gallery.php
@@ -0,0 +1,17 @@
+<?php include ($_SERVER["DOCUMENT_ROOT"]."/frames/headtags.php"); 
+echo "<title>$titleconst Галерея</title>";
+?>
+
+<meta name="description" content="Галерея клиентов магазина autobagaz.ru Пермь, ул. Спешилова 102/29.">
+<meta name="keywords" content="автобагажники, автомобильные багажники, багажники для автомобиля, прокат, прокат багажников, прокат боксов, багажные системы, багажник на крышу, автобагажники на крышу, автомобильные боксы, автобоксы, автобокс, бокс, бокс на крышу, автобокс на крышу, велобагажники, велокрепления, багажники для велосипедов, велокрепления на крышу, велокрепления на фаркопы, крепления для лыж, крепления для сноубордов, рейлинги, интернет-магазин, продажа в Перми, Пермь, Автобагаж, в Перми, thule, туле, Yuago, ЯГО, Турино, Lux, Люкс, Атлант, Discovery, Amos, Амос, Mont Blanc, Монблан, Can, Voyager, Cosmo, Муравей, Атера, Atera, menabo"> 
+</head>
+<body>
+<?php include ($_SERVER["DOCUMENT_ROOT"]."/frames/header.html");?>
+<div id="leftmenu">
+<?php include ($_SERVER["DOCUMENT_ROOT"]."/frames/leftmenu.html");?>
+</div>
+<div id="content">
+
+</div>
+<?php include ($_SERVER["DOCUMENT_ROOT"]."/frames/footer.html");?>
+<?php include ($_SERVER["DOCUMENT_ROOT"]."/frames/counters.html");?>
