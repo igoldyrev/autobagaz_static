bf4b64b2f6016a274d08b3f41fd49632d7e85ad6
igoldyrev <goldirev12@gmail.com>
2017-06-25 13:48:16 +0500

    Убрана интеграция с БД, создан массив с данными (1 юнит), проведен тест вывода данных

--- gallery.php
+++ gallery.php
@@ -12,91 +12,34 @@ echo "<title>$titleconst Галерея</title>";
 </div>
 <div id="content">
 <?php
+$gallery = [
+		[
+		'id' => 1,
+		'name' => 'Lada Xray',
+		'img1' => '<img class="img_main" src="/images/gallery/20170303_1_xray.jpg" srcset="/images/gallery/20170303_1_xray.jpg 900w" alt="Lada Xray" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+		'img2' => '<img class="img_main" src="/images/gallery/20170303_2_xray.jpg" srcset="/images/gallery/20170303_2_xray.jpg 600w" alt="Lada Xray" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+		'img3' => '<img class="img_main" src="/images/gallery/20170303_3_xray.jpg" srcset="/images/gallery/20170303_3_xray.jpg 600w" alt="Lada Xray" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+		'img4' => '<img class="img_main" src="/images/gallery/20170303_4_xray.jpg" srcset="/images/gallery/20170303_4_xray.jpg 600w" alt="Lada Xray" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+		'img5' => '<img class="img_main" src="/images/gallery/20170303_5_xray.jpg" srcset="/images/gallery/20170303_5_xray.jpg 500w" alt="Lada Xray" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+		'img6' => '<img class="img_main" src="/images/gallery/20170303_6_xray.jpg" srcset="/images/gallery/20170303_6_xray.jpg 700w" alt="Lada Xray" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+		],
+		
 
-// Соединение с БД MySQL
-$sql = mysql_connect('localhost', '9082410193', 'GfhjkmDatabase');
-mysql_select_db('9082410193_gallery', $sql);
+];
 
-
-mysql_query ("set_client='utf8'");//Следующие 2 строки решают проблему с кодировкой.
-mysql_query ("SET NAMES utf8");
-
-// Количество новостей на странице
-$on_page = 10;
-
-
-// Получаем количество записей таблицы news
-$query = "SELECT COUNT(*) FROM `photos`";
-$res = mysql_query($query);
-$count_records = mysql_fetch_row($res);
-$count_records = $count_records[0];
-
-
-// Получаем количество страниц
-// Делим количество записей на количество новостей на странице
-// и округляем в большую сторону
-$num_pages = ceil($count_records / $on_page);
-
-
-// Текущая страница из GET-параметра page
-// Если параметр не определен, то текущая страница равна 1
-$current_page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
-
-// Если текущая страница меньше единицы, то страница равна 1
-if ($current_page < 1)
-{
-    $current_page = 1;
-}
-// Если текущая страница больше общего количества страница, то
-// текущая страница равна количеству страниц
-elseif ($current_page > $num_pages)
-{
-    $current_page = $num_pages;
-}
-
-
-// Начать получение данных от числа (текущая страница - 1) * количество записей на странице
-$start_from = ($current_page - 1) * $on_page;
-
-
-// Формат оператора LIMIT <ЗАПИСЬ ОТ>, <КОЛИЧЕСТВО ЗАПИСЕЙ>
-$query = "SELECT `id`, `name`, `img1`, `img2`, `img3`, `img4`, `img5`, `img6`, `img7`, `img8`, `img9`, `img10`, `img_alt` FROM `photos` ORDER BY `id` DESC LIMIT $start_from, $on_page";
-$res = mysql_query($query);
-
-
-// Вывод результатов
-while ($row = mysql_fetch_assoc($res))
-{
-    echo '<h2>'.$row['name'].'</h2>';
-    echo '<div class="img_div">';
-    echo '<img class="img_main" src="'.$row['img1'].'" srcset="'.$row['img1'].' 200w" alt="'.$row['img_alt'].'" sizes="(max-width: 2000px) 150px, 300px, 350px">';
-    echo '<img class="img_main" src="'.$row['img2'].'" srcset="'.$row['img2'].' 200w" alt="'.$row['img_alt'].'" sizes="(max-width: 2000px) 150px, 300px, 350px">';
-	echo '<img class="img_main" src="'.$row['img3'].'" srcset="'.$row['img3'].' 200w" alt="'.$row['img_alt'].'" sizes="(max-width: 2000px) 150px, 300px, 350px">';
-	echo '<img class="img_main" src="'.$row['img4'].'" srcset="'.$row['img4'].' 200w" alt="'.$row['img_alt'].'" sizes="(max-width: 2000px) 150px, 300px, 350px">';
-	echo '<img class="img_main" src="'.$row['img5'].'" srcset="'.$row['img5'].' 200w" alt="'.$row['img_alt'].'" sizes="(max-width: 2000px) 150px, 300px, 350px">';
-	echo '<img class="img_main" src="'.$row['img6'].'" srcset="'.$row['img6'].' 200w" alt="'.$row['img_alt'].'" sizes="(max-width: 2000px) 150px, 300px, 350px">';
-	echo '<img class="img_main" src="'.$row['img7'].'" srcset="'.$row['img7'].' 200w" alt="'.$row['img_alt'].'" sizes="(max-width: 2000px) 150px, 300px, 350px">';
-	echo '<img class="img_main" src="'.$row['img8'].'" srcset="'.$row['img8'].' 200w" alt="'.$row['img_alt'].'" sizes="(max-width: 2000px) 150px, 300px, 350px">';
-	echo '<img class="img_main" src="'.$row['img9'].'" srcset="'.$row['img9'].' 200w" alt="'.$row['img_alt'].'" sizes="(max-width: 2000px) 150px, 300px, 350px">';
-	echo '<img class="img_main" src="'.$row['img10'].'" srcset="'.$row['img10'].' 200w" alt="'.$row['img_alt'].'" sizes="(max-width: 2000px) 150px, 300px, 350px">';
-	echo '</div>';
-}
-
-// Вывод списка страниц
-echo '<p>';
-for ($page = 1; $page <= $num_pages; $page++)
-{
-    if ($page == $current_page)
-    {
-        echo '<strong>'.$page.'</strong> &nbsp;';
-    }
-    else
-    {
-        echo '<a href="gallery.php?page='.$page.'">'.$page.'</a> &nbsp;';
-    }
-}
-echo '</p>';
-?>
+foreach ($gallery as $photos): ?>
+	<h3><?php echo $photos['name']; ?></h3>
+	<div class="img_div">
+		<?php echo $photos['img1']; ?>
+		<?php echo $photos['img2']; ?>
+		<?php echo $photos['img3']; ?> 
+		<?php echo $photos['img4']; ?> 
+		<?php echo $photos['img5']; ?> 
+		<?php echo $photos['img6']; ?> 
+		<?php echo $photos['img7']; ?>
+		<?php echo $photos['img8']; ?> 
+	</div>
+<?php endforeach; ?>
 </div>
 <?php include ($_SERVER["DOCUMENT_ROOT"]."/frames/footer.html");?>
 <?php include ($_SERVER["DOCUMENT_ROOT"]."/frames/counters.html");?>
