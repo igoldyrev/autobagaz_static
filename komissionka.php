d969dc81d6bd084f0675c514e45fec22824f5dfd
igoldyrev <goldirev12@gmail.com>
2017-07-14 12:30:51 +0500

    Созданы массивы с данными для раздела

--- komissionka.php
+++ komissionka.php
@@ -11,6 +11,71 @@ echo "<title>$titleconst Комиссионка</title>";
 <?php include ($_SERVER["DOCUMENT_ROOT"]."/frames/leftmenu.html");?>
 </div>
 <div id="content">
+<?php
+$bagazhniki = [
+		[
+			'id' => '1',
+			'name' => 'Багажник Д-1, состояние отличное, после использования, цвет черный.',
+			'img1' => '<img class="img_main" src="/images/komissionka/d1-1.jpg" srcset="
+			/images/komissionka/d1-1.jpg 2500w" sizes="(max-width: 2000px) 150px, 300px, 350px" alt="Д-1">',
+			'img2' => '<img class="img_main" src="/images/komissionka/d1-2.jpg" srcset="
+			/images/komissionka/d1-2.jpg 2500w" sizes="(max-width: 2000px) 150px, 300px, 350px" alt="Д-1">',
+			'price' => 'Цена 2500 рублей',
+		],
+		[
+			'id' => '2',
+			'name' => 'Багажник на Renault Logan/Sandero, состояние удовлетворительное, после использования, цвет черный.',
+			'img1' => '<img class="img_main" src="/images/komissionka/logan-1.jpg" srcset="
+			/images/komissionka/logan-1.jpg 3000w" sizes="(max-width: 2000px) 150px, 300px, 350px" alt="багажник для Renault Logan/Sandero">',
+			'img2' => '<img class="img_main" src="/images/komissionka/logan-2.jpg" srcset="
+			/images/komissionka/logan-2.jpg 3000w" sizes="(max-width: 2000px) 150px, 300px, 350px" alt="багажник для Renault Logan/Sandero">',
+			'price' => 'Цена 900 рублей',
+		],
+		[
+			'id' => '3',
+			'name' => 'Аэродинамические дуги для рейлингов, состояние хорошее, цвет черный.',
+			'img1' => '<img class="img_main" src="/images/komissionka/aero-1.jpg" srcset="
+			/images/komissionka/aero-1.jpg 3000w" sizes="(max-width: 2000px) 150px, 300px, 350px" alt="Аэродинамические дуги для рейлингов">',
+			'img2' => '<img class="img_main" src="/images/komissionka/aero-2.jpg" srcset="
+			/images/komissionka/aero-2.jpg 3000w" sizes="(max-width: 2000px) 150px, 300px, 350px" alt="Аэродинамические дуги для рейлингов">',
+			'img3' => '<img class="img_main" src="/images/komissionka/aero-3.jpg" srcset="
+			/images/komissionka/aero-3.jpg 3000w" sizes="(max-width: 2000px) 150px, 300px, 350px" alt="Аэродинамические дуги для рейлингов">',
+			'price' => 'Цена 1500 рублей',
+		],
+];
+$farkops = [
+		[
+			'id' => '1',
+			'name' => 'Фаркоп на Mitsubishi Lanser 10, 2007-2012, новый.',
+			'img1' => '<img class="img_main" src="/images/komissionka/farkop-mitsubisi-1.jpg" srcset="
+			/images/komissionka/farkop-mitsubisi-1.jpg 3000w" sizes="(max-width: 2000px) 150px, 300px, 350px" alt="Фаркоп для Mitsubishi Lanser">',
+			'img2' => '<img class="img_main" src="/images/komissionka/farkop-mitsubisi-2.jpg" srcset="
+			/images/komissionka/farkop-mitsubisi-2.jpg 3000w" sizes="(max-width: 2000px) 150px, 300px, 350px" alt="Фаркоп для Mitsubishi Lanser">',
+			'img3' => '<img class="img_main" src="/images/komissionka/farkop-mitsubisi-3.jpg" srcset="
+			/images/komissionka/farkop-mitsubisi-3.jpg 3000w" sizes="(max-width: 2000px) 150px, 300px, 350px" alt="Фаркоп для Mitsubishi Lanser">',
+			'img4' => '<img class="img_main" src="/images/komissionka/farkop-mitsubisi-4.jpg" srcset="
+			/images/komissionka/farkop-mitsubisi-4.jpg 3000w" sizes="(max-width: 2000px) 150px, 300px, 350px" alt="Фаркоп для Mitsubishi Lanser">',
+			'price' => 'Цена 6000 рублей',
+		],
+];
+$korzins = [
+		[
+			'id' => '1',
+			'name' => 'Корзина для перевозки грузов. Крепится к водостокам. Подходит для большинства автомобилей семейства ВАЗ, Нива.',
+			'img1' => '<img class="img_main" src="/images/komissionka/korzina-1.jpg" srcset="
+			/images/komissionka/korzina-1.jpg 3000w" sizes="(max-width: 2000px) 150px, 300px, 350px" alt="Корзина для перевозки грузов">',
+			'img2' => '<img class="img_main" src="/images/komissionka/korzina-2.jpg" srcset="
+			/images/komissionka/korzina-2.jpg 3000w" sizes="(max-width: 2000px) 150px, 300px, 350px" alt="Корзина для перевозки грузов">',
+			'img3' => '<img class="img_main" src="/images/komissionka/korzina-3.jpg" srcset="
+			/images/komissionka/korzina-3.jpg 3000w" sizes="(max-width: 2000px) 150px, 300px, 350px" alt="Корзина для перевозки грузов">',
+			'price' => 'Цена 2000 рублей',
+		],
+];
+
+
+?>
+
+
 <h1>Комиссионка</h1>
 <p>В этом разделе мы продаем б/у багажники, автобоксы и аксессуары в рабочем состоянии. Часть багажников - использовались в прокате, часть - были оставлены на реализацию нашими клиентами. В течении 2-х недель вы можете вернуть багажник, если он вам не подойдет или окажется неисправен.</p>
 <p>Если у вас есть ненужный багажник - мы его купим.</p>
